8eec2b111dd996e658c39f9d580aac993a086d10
TheTechieYeti <elliott.scott.t@gmail.com>
2021-12-17 15:20:19 -0800

    Change order of if statements

--- Algorithms/Fizzbuzz.js
+++ Algorithms/Fizzbuzz.js
@@ -1,17 +1,18 @@
 function fizzbuzz(){
     console.log("hi")
     for(var i=1;i<=100;i++){
+        var output = i
+        
+        if(i%3==0){
+            output = "Fizz"
+            }
+        if(i%5==0){
+            output = "Buzz"
+            }
         if(i%15==0){
-            console.log("FizzBuzz")
-        }
-        else{
-            if(i%3==0){
-                console.log("Fizz")
-            }
-            if(i%5==0){
-                console.log("Buzz")
-            }
-        }
+            output = "FizzBuzz" 
+            }    
+        console.log (output)
         
     }
 }
